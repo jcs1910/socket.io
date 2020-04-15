6e4a57d79793df777ccfd6e9d4bec129d83b08d0
dokdo <jcs191072@gmail.com>
2020-04-16 01:42:56 +0900

    Two-way communication between sever and brower + email notifications

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '../public')
+
+let app = express();
+
+app.use(express.static(publicPath));
+
+let server = http.createServer(app);
+
+let io = socketIO(server); //io는 브라우저가 전달하는 모든 이벤트를 서버쪽에서 처리
+
+io.on('connection', (socket) => {
+  console.log('A new browser has connected to this server.');
+  
+  socket.on('changSukEmail', () => {
+    console.log('New email from Changsuk')
+  })
+
+  io.emit('toChangsukEmail', {
+    from: 'Korea@naver.com',
+    content: `Hello. From Korea`
+  }) 
+})
+
+server.listen(3000, () => {
+  console.log('Server is running...')
+})
